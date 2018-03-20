93d800d0481e9dfa40f690e2ad152d15080eb075
Mohammed Manahi <M.Menahi@gmail.com>
2018-03-20 18:36:29 +0200

    style static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
+     @disable_page_header = true
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
-  get 'static_pages/index'
-
-  get 'static_pages/about'
+  get '/index', to: 'static_pages#index'
+  get '/about', to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
