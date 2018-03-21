1387b671f60f19e075672be5f65d58d115555335
Mohammed Manahi <M.Menahi@gmail.com>
2018-03-21 17:23:31 +0200

    fix layers on landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :config_premitted_params, if: :devise_controller?
+
   protected
+
   # customize devise registration, add first_name and last_name to sign up
   def config_premitted_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
